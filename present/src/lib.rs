99526c4c703a4abbad62f5347facf19a30e5417f
Jeroen van Maanen <jeroen@leialearns.org>
2024-03-05 22:05:37 +0100

    Improved module `present`

--- present/src/lib.rs
+++ present/src/lib.rs
@@ -1,7 +1,9 @@
 use log::{error, info};
 use tonic_web_wasm_client::Client;
 use wasm_bindgen::prelude::wasm_bindgen;
+use wasm_bindgen::{JsCast, UnwrapThrowExt};
 use wasm_bindgen_futures::spawn_local;
+use web_sys::HtmlInputElement;
 use yew::prelude::*;
 
 pub mod proto_example {
@@ -13,11 +15,24 @@ use crate::proto_example::Greeting;
 
 #[function_component]
 fn GreeterApp() -> Html {
+    let greeting = use_state(|| "".to_string());
+    let oninput = {
+        let greeting = greeting.clone();
+        move |input_event: InputEvent| {
+            let target: HtmlInputElement = input_event
+                .target()
+                .unwrap_throw()
+                .dyn_into()
+                .unwrap_throw();
+            greeting.set(target.value());
+        }
+    };
     let counter = use_state(|| 0);
     let onclick = {
-        spawn_local(call_greeter());
+        let greeting = greeting.clone();
         let counter = counter.clone();
         move |_| {
+            spawn_local(call_greeter((*greeting).clone()));
             let value = *counter + 1;
             counter.set(value);
         }
@@ -25,16 +40,24 @@ fn GreeterApp() -> Html {
 
     html! {
         <div>
-            <button {onclick}>{ "+1" }</button>
-            <p>{ *counter }</p>
-            <a href={"https://github.com/rustigaan/full-stack/tree/wasm"}>{"GitHub"}</a>
+            <div>
+                <input type={"text"} {oninput}/>
+                <button {onclick}>{ "+1" }</button>
+                <p>{ *counter }</p>
+            </div>
+            <div>
+                {&*greeting}
+            </div>
+            <div class={"footer"}>
+                <a href={"https://github.com/rustigaan/full-stack/tree/wasm"}>{"🦀 GitHub 🦀"}</a>
+            </div>
         </div>
     }
 }
 
-async fn call_greeter() -> () {
+async fn call_greeter(greeting: String) -> () {
     info!("Call greeter");
-    let greeting = Greeting { message: "Hello, World".to_string() };
+    let greeting = Greeting { message: greeting };
     let base_url = "http://localhost:3000"; // URL of the gRPC-web server
     let mut greeter_client = GreeterServiceClient::new(Client::new(base_url.to_string()));
     match greeter_client.greet(greeting).await {
