a38ef67e131d1504b97f714ad0c44618747abc14
Jeroen van Maanen <jeroen@leialearns.org>
2024-03-05 21:20:29 +0100

    Point URL to exported port of Docker compose cluster

--- present/src/lib.rs
+++ present/src/lib.rs
@@ -1,4 +1,4 @@
-use log::info;
+use log::{error, info};
 use tonic_web_wasm_client::Client;
 use wasm_bindgen::prelude::wasm_bindgen;
 use wasm_bindgen_futures::spawn_local;
@@ -35,13 +35,13 @@ fn GreeterApp() -> Html {
 async fn call_greeter() -> () {
     info!("Call greeter");
     let greeting = Greeting { message: "Hello, World".to_string() };
-    let base_url = "http://localhost:9001"; // URL of the gRPC-web server
+    let base_url = "http://localhost:3000"; // URL of the gRPC-web server
     let mut greeter_client = GreeterServiceClient::new(Client::new(base_url.to_string()));
     match greeter_client.greet(greeting).await {
         Ok(response) => {
             let ack = response.into_inner();
             info!("Response: {ack:?}") },
-        Err(err) => info!("Error: {err:?}"),
+        Err(err) => error!("Error: {err:?}"),
     };
 }
 
